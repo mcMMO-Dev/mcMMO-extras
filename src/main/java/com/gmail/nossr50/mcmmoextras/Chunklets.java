9790784e1889f1644f29f42021eca734dcb1fa1e
NuclearW <incongruency@gmail.com>
2012-07-08 00:09:21 -0400

    Start Chunklet analysis component

---
+++ src/main/java/com/gmail/nossr50/mcmmoextras/Chunklets.java
@@ -0,0 +1,92 @@
+package com.gmail.nossr50.mcmmoextras;
+
+import java.io.File;
+import java.util.ArrayList;
+
+public class Chunklets {
+	public static void analyze(String worldLocation) {
+		File worldDir = new File(worldLocation);
+		if(!worldDir.exists()) {
+			System.out.println("Cannot find world at: " + worldDir.getPath());
+			return;
+		}
+
+		File chunkletsDir = new File(worldLocation, "mcmmo_data");
+		if(!chunkletsDir.exists()) {
+			System.out.println("Cannot find mcmmo_data folder in: " + worldDir.getPath());
+			return;
+		}
+
+		// Scan
+
+		System.out.println("Scanning mcmmo_data: ");
+		Main.updateProgress(0);
+
+		ArrayList<String> unknownFiles = new ArrayList<String>();
+
+		int cxDirectories = 0;
+		int emptyCxDirectories = 0;
+		int czDirectories = 0;
+		int emptyCzDirectories = 0;
+
+		int chunkletsCount = 0;
+
+		ArrayList<String> chunkletLocations = new ArrayList<String>();
+
+		String[] cxDirs = chunkletsDir.list();
+		for(int i = 0; i < cxDirs.length; i++) {
+			File cxDir = new File(chunkletsDir, cxDirs[i]);
+
+			if(!cxDir.isDirectory()) {
+				unknownFiles.add(cxDir.getPath());
+				continue;
+			}
+			cxDirectories++;
+
+			String[] czDirs = cxDir.list();
+			int emptyCzTemp = 0;
+			for(int j = 0; j < czDirs.length; j++) {
+				File czDir = new File(cxDir, czDirs[j]);
+
+				if(!czDir.isDirectory()) {
+					unknownFiles.add(czDir.getPath());
+					continue;
+				}
+				czDirectories++;
+
+				String[] chunklets = czDir.list();
+				if(chunklets.length == 0) {
+					emptyCzTemp++;
+					emptyCzDirectories++;
+				}
+
+				for(int k = 0; k < chunklets.length; k++) {
+					File chunklet = new File(czDir, chunklets[k]);
+
+					chunkletLocations.add(chunklet.getPath());
+					chunkletsCount++;
+				}
+			}
+
+			if(emptyCzTemp == czDirs.length) {
+				emptyCxDirectories++;
+			}
+
+			Main.updateProgress((double) i / cxDirs.length);
+		}
+		Main.updateProgress(1);
+		System.out.println();
+
+		System.out.println("Found " + chunkletsCount + " chunklets:");
+		System.out.println("\t" + cxDirectories + " cx dirs, " + emptyCxDirectories + " empty");
+		System.out.println("\t" + czDirectories + " cz dirs, " + emptyCzDirectories + " empty");
+
+		if(!unknownFiles.isEmpty()) {
+			System.out.println("The following files not part of the chunklet system were also found:");
+			for(String unknown : unknownFiles) {
+				System.out.println("\t" + unknown);
+			}
+		}
+
+	}
+}
--- src/main/java/com/gmail/nossr50/mcmmoextras/Main.java
+++ src/main/java/com/gmail/nossr50/mcmmoextras/Main.java
@@ -2,6 +2,46 @@ package com.gmail.nossr50.mcmmoextras;
 
 public class Main {
 	public static void main(String[] args) {
-		System.out.println("I fight for the user!");
+		if (args.length == 0) {
+			printBaseHelp();
+			return;
+		}
+
+		if (args[0].equalsIgnoreCase("chunklets")) {
+			if (args.length < 3) {
+				printChunkletHelp();
+				return;
+			}
+			if (args[2].equalsIgnoreCase("analyze")) {
+				Chunklets.analyze(args[1]);
+				return;
+			}
+		}
+	}
+
+	public static void updateProgress(double progressPercentage) {
+		final int width = 50; // progress bar width in chars
+
+		System.out.print("\r[");
+		int i = 0;
+		for (; i <= (int) (progressPercentage * width) - 1; i++) {
+			System.out.print("=");
+		}
+		for (; i < width; i++) {
+			System.out.print(" ");
+		}
+		System.out.print("] ");
+
+		System.out.print((int) (progressPercentage * 100) + "%");
+	}
+
+	private static void printChunkletHelp() {
+		System.out.println("chunklets <world_folder> analyze - Print information about Chunklets for a given world");
+	}
+
+	private static void printBaseHelp() {
+		System.out.println("Use the following arguments for more information: ");
+		System.out.println("\tchunklets - Print help for Chunklet related commands");
+		System.out.println("\t? - Print this help");
 	}
 }
